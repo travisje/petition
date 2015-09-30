e8c03d012564e8d73f3c62ab463327015667a548
travisje <travisje@gmail.com>
2015-09-29 20:09:14 -0400

    add country list, form func, and flash msgs

--- app/controllers/signers_controller.rb
+++ app/controllers/signers_controller.rb
@@ -1,6 +1,33 @@
 class SignersController < ApplicationController
+  def new
+    @signer = Signer.new
+  end
+
+  def create
+    @signer = Signer.new(signer_params)
+    if @signer.save
+      redirect_to signer_path(@signer)
+    elsif Signer.find_by(email: params[:signer][:email])
+      flash.now[:alert] = "The petition has already been signed by a user with this email. Please enter a valid email address."
+      render 'new'
+    else
+      binding.pry
+      flash.now[:alert] = "Please enter a valid email address."
+      render 'new'
+    end
+  end
+
+  def show
+    @signer = Signer.find(params[:id])
+  end
+  
   def index
 
   end
-  
+
+  private
+    def signer_params 
+      params.require(:signer).permit(:first_name, :last_name, :email, :country, :street_address, :zip, :comment, :display_sig, :showcase)
+    end
+
 end
--- app/models/signer.rb
+++ app/models/signer.rb
@@ -1,2 +1,9 @@
 class Signer < ActiveRecord::Base
+  validates :first_name, :last_name, :country, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, presence: true, uniqueness: {case_sensitive: false}
+  
+  def email=(email)
+    write_attribute(:email, email.downcase)
+  end
+
 end
