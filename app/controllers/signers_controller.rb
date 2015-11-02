44c6caba3eaf637ee61e2f543f7718efe1061316
travisje <travisje@gmail.com>
2015-11-01 19:50:35 -0500

    Make PG fixes and fix signer controller

--- app/controllers/signers_controller.rb
+++ app/controllers/signers_controller.rb
@@ -9,7 +9,7 @@ class SignersController < ApplicationController
     if @signer.save
       # mailchimp = MailChimp.new
       # mailchimp.subscribe(@signer)
-      GoogleSheet.new.spreadsheet.add_record(@signer)
+      GoogleSheet.new.add_record(@signer)
       render 'success'
     else 
       flash.now[:alert] = @signer.pretty_errors
