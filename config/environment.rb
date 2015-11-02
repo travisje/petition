c567174c30949606c1f4c98443f68d9b0787e4ed
travisje <travisje@gmail.com>
2015-11-02 09:59:30 -0500

    Remove cacert for heroku test

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ENV['SSL_CERT_FILE'] = Gem.loaded_specs['google-api-client'].full_gem_path+'/lib/cacerts.pem'
