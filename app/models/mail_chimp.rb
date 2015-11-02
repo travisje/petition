b1ea11cf96b81a1340e61f5897d2802e77323c83
travisje <travisje@gmail.com>
2015-11-02 10:48:04 -0500

    Fix mailchimp api call

--- app/models/mail_chimp.rb
+++ app/models/mail_chimp.rb
@@ -5,11 +5,11 @@ class MailChimp
   attr_accessor :mailchimp
   
   def initialize
-    @mailchimp = Mailchimp::API.new(ENV['mailchimp-api-key'])
+    @mailchimp = Mailchimp::API.new(ENV['mailchimp_api_key'])
   end
 
   def subscribe(signer)
-    mailchimp.lists.subscribe(ENV['mailchimp-list_id'], {:email => signer.email}, {"FNAME" => signer.first_name, "LNAME" => signer.last_name}, "HTML", false, false, false, true)
+    mailchimp.lists.subscribe(ENV['mailchimp_list_id'], {:email => signer.email}, {"FNAME" => signer.first_name, "LNAME" => signer.last_name}, "HTML", false, false, false, true)
   end
 
   
