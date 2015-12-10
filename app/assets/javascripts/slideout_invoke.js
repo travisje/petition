5d84bd3e31bde0b4a3cfa1a1d651d067e4db1947
travisje <travisje@gmail.com>
2015-12-10 16:36:26 -0500

    replace slidout jquery with media queries

--- app/assets/javascripts/slideout_invoke.js
+++ app/assets/javascripts/slideout_invoke.js
@@ -1,56 +1,17 @@
 $(document).ready(function() {
   
-  var threshold = 840;
-  var currentWindowSize = $(window).width();
-  var currentLoadState = desktopOrMobile(currentWindowSize);
-
-  function show_desktop() {
-      $('#bignav').show();
-      $('#menu').hide();
-      $('#smallnav').hide();
-  }
-
-  function show_mobile() {
-    $('#bignav').hide();
-    $('#menu').show().css("display","");
-    $('#smallnav').show();
-    var slideout = new Slideout({
-      'panel': document.getElementById('panel'),
-      'menu': document.getElementById('menu'),
-      'padding': 180,
-      'side': 'right',
-      'tolerance': 70,
-      'touch': false
-    });
-    // Toggle button
-    document.querySelector('.toggle-button').addEventListener('click', function() {
-      slideout.toggle();
-    });
-  }
-
-  function desktopOrMobile (size) {
-    if (size > threshold) {
-      return "desktop";
-    }
-    else {return "mobile";}
-  }
-
-  if ( $(window).width() > threshold) {
-    show_desktop();
-  }
-  else {
-    show_mobile();
-  }
-
-  $(window).on('resize',function(e){
-    var current_width = $(window).width();
-    if (currentLoadState === "desktop" && current_width <= threshold) {
-      show_mobile();
-      currentLoadState = "mobile";
-    }
-    if (currentLoadState === "mobile" && current_width > threshold) {
-      show_desktop();
-      currentLoadState = "desktop";
-    }
+  var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 180,
+    'side': 'right',
+    'tolerance': 70,
+    'touch': false
   });
+
+  // Toggle button
+  document.querySelector('.toggle-button').addEventListener('click', function() {
+    slideout.toggle();
+  });
+  
 });
