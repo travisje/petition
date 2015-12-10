bdb0d5d9aaf8245a6d60b5fe9d5c7c6266e3a4df
travisje <travisje@gmail.com>
2015-12-10 11:55:31 -0500

    decrease slideout width and make no touch

--- app/assets/javascripts/slideout_invoke.js
+++ app/assets/javascripts/slideout_invoke.js
@@ -17,9 +17,10 @@ $(document).ready(function() {
     var slideout = new Slideout({
       'panel': document.getElementById('panel'),
       'menu': document.getElementById('menu'),
-      'padding': 256,
+      'padding': 180,
       'side': 'right',
-      'tolerance': 70
+      'tolerance': 70,
+      'touch': false
     });
     // Toggle button
     document.querySelector('.toggle-button').addEventListener('click', function() {
